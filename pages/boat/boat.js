adc8bf3877d53c38a5fb3c77d47fc3cf3b17444c
liurongjie <1397608894@qq.com>
2019-04-26 18:59:19 +0800

    no message

--- pages/boat/boat.js
+++ pages/boat/boat.js
@@ -1,18 +1,181 @@
-// pages/boat/boat.js
+// pages/class/class.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    classfiySelect: "",
+    leftText: [{
+      id: "01",
+      text1: "美妆专区",
+    },
+    {
+      id: "02",
+      text1: "面部清洁",
+    },
+    {
+      id: "03",
+      text1: "洗护专区",
+    },
+    {
+      id: "04",
+      text1: "面膜",
+    },
+   
+    
 
+    ],
+    rightData: [{
+      id: "01",
+      title: "美妆专区",
+      frist: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+        id: 1,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+        id: 2,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      id: "02",
+      title: "面部清洁",
+      frist: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      id: "03",
+      title: "洗护专区",
+      frist: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      id: "04",
+      title: "面膜",
+      frist: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      id: "05",
+      title: "口红",
+      frist: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      id: "06",
+      title: "美妆专区",
+      frist: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      classfiySelect: this.data.leftText[0].id
+    })
   },
 
   /**
@@ -61,6 +224,47 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+  //滚动触发
+  scroll: function (e) {
+    var scrollTop = e.detail.scrollTop,
+      h = 0,
+      classfiySelect;
+    var that = this;
+    that.data.leftText.forEach(function (clssfiy, i) {
+      var _h = 26 + that.length(clssfiy['id']) * 102;
+      if (scrollTop >= h) {
+        classfiySelect = clssfiy['id'];
+      }
+      h += _h;
+      console.log(h);
+    })
+    that.setData({
+      classfiySelect: classfiySelect,
+    })
+  },
+  //求每一栏高度
+  length: function (e) {
+    var that = this;
+    var rightData = that.data.rightData;
+    for (var i = 0; i < rightData.length; i++) {
+      if (rightData[i]['id'] == e) {
+        return rightData[i]['frist'].length;
+
+      }
+    }
+  },
+  //点击左边事件
+  left_list: function (e) {
+    var that = this;
+    var l_id = e.currentTarget.dataset.id;
+    that.setData({
+      rigId: l_id,
+    })
+  },
+  //跳转详情界面
+  particulars: function (e) {
 
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -5,6 +5,123 @@ Page({
    * 页面的初始数据
    */
   data: {
+    classfiySelect:1,
+    length:[],
+    left:[
+      {
+        type:1,
+        name:"健身"
+      },
+      {
+        type: 2,
+        name: "驾校"
+      },
+      {
+        type: 3,
+        name: "考研"
+      },
+      {
+        type: 4,
+        name: "小语种"
+      },
+    ],
+    right: [{
+      type: 1,
+      title: "健身",
+      data: [{
+        url: "/images/85309.jpg",
+        text: "宇动",
+        money: 200,
+        sum: 20,
+        id: 1,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "宇动",
+        money: 200,
+        sum: 20,
+        id: 2,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "宇动",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      type: 2,
+      title: "驾校",
+      data: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      type: 3,
+      title: "考研",
+      data: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    {
+      type: 4,
+      title: "小语种",
+      data: [{
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      {
+        url: "/images/85309.jpg",
+        text: "卡姿兰补水套装",
+        money: 200,
+        sum: 20,
+      },
+      ],
+    },
+    
+
+    ],
 
   },
 
@@ -26,6 +143,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    this.length();
 
   },
 
@@ -62,5 +180,65 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  selecttype: function (e) { 
+    this.setData({
+      classfiySelect: e.currentTarget.dataset.type,
+    })
+    
+
+  },
+
+  length: function (e) {
+    
+    var that = this;
+    var rightData = that.data.right;
+    console.log(rightData)
+    var length=[];
+    for (var i = 0; i < rightData.length; i++) {
+      if(i==0){
+        console.log(rightData[i].data.length)
+        length[i] = rightData[i].data.length *100 + 20;
+
+      }
+      else{
+        console.log(rightData[i].data.length)
+        length[i] = rightData[i].data.length * 100 + 20+length[i-1];
+      }
+     
+    }
+    this.setData({
+      length:length,
+    })
+    console.log(length)
+  },
+  scroll: function (e) {
+    var scrollTop = e.detail.scrollTop,
+      h = 0,
+      classfiySelect;
+    var that = this;
+    console.log(scrollTop)
+    var length=this.data.length;
+    for (var i = length.length-1;i>=0;i--){
+      if(scrollTop>length[i]){
+        console.log(i)
+       this.setData({
+         classfiySelect:i+2,
+       })
+        return '';
+
+      }
+    }
+    // that.data.left.forEach(function (clssfiy, i) {
+    //   var _h = 26 + that.length1(clssfiy['id']) * 102;
+    //   if (scrollTop >= h) {
+    //     classfiySelect = clssfiy['type'];
+    //   }
+    //   h += _h;
+    //   console.log(h);
+    // })
+    // that.setData({
+    //   classfiySelect: classfiySelect,
+    // })
+  },
 })
